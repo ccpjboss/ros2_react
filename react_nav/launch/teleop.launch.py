903c2264f9aba83d44306f1f295bd192a8b73bf0
joaocastilho <joaocastilho@pop-os.localdomain>
2021-09-13 11:53:44 +0100

    first commit

---
+++ react_nav/launch/teleop.launch.py
@@ -0,0 +1,52 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import TextSubstitution
+from launch.substitutions import EnvironmentVariable
+import launch.actions
+
+
+def generate_launch_description():
+    use_sim_time = launch.substitutions.LaunchConfiguration('use_sim_time', default='true')
+    return LaunchDescription([
+        Node(
+            package='stage_ros2',
+            namespace='/',
+            executable='stage_ros2',
+            name='stage',
+            parameters=[
+                {"world": "./src/react_nav/worlds/rooms.world"}
+            ],
+            remappings=[
+                ("/stage/ranger_0", "/scan"),
+                ("/stage/odom", "/odom")
+            ]
+        ),
+        Node(
+            package='teleop_twist_keyboard',
+            namespace='/',
+            executable='teleop_twist_keyboard',
+            name='teleop',
+            remappings=[
+                ("/cmd_vel", "/stage/cmd_vel")
+            ],
+            output='screen',
+            prefix='xterm -e'
+        ),
+        Node(
+            package='slam_gmapping',
+            namespace='/',
+            executable='slam_gmapping',
+            output='screen',
+            name='slam',
+            parameters=[
+                {'use_sim_time':use_sim_time}
+            ]
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            output='screen',
+            name='rviz'
+        )
+    ])
---
+++ react_nav/src/reactive_navigation.cpp
@@ -0,0 +1,95 @@
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/laser_scan.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+
+#include <chrono>
+#include <memory>
+#include <functional>
+#include <iostream>
+
+using namespace std::chrono_literals;
+
+class ReactiveController : public rclcpp::Node{
+private:
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_pub_;
+    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laser_scan_sub_;
+
+    double obstacle_distance;
+    bool robot_stopped=false; 
+
+    rclcpp::Time rotate_start;     //Time when the rotation starts
+    int rotation_orientation;
+
+    rclcpp::TimerBase::SharedPtr timer_;
+
+    geometry_msgs::msg::Twist calculateCommand()
+    {
+        auto msg = geometry_msgs::msg::Twist();
+        
+        //If the robot is not near an obstacle and it is not stopped
+        if(obstacle_distance > 0.5 && this->robot_stopped == false){
+            msg.linear.x = 1.0;
+        }else{ //If the robot is near an obstacle
+            int32_t nsec = rand()%9;
+            rclcpp::Duration rotate_time(rand()%2,nsec*exp10(8));
+            if (this->robot_stopped == false) //If the robot was moving
+            {
+                //The robot will now stop
+                this->robot_stopped = true;
+
+                //Gets the time variables for the rotation
+                this->rotate_start = now();
+                //rotate_time = rclcpp::Duration(rand()%2,nsec*exp10(8));
+
+                RCLCPP_INFO(this->get_logger(),"Rotation time: %d.%d",rotate_time.seconds(),rotate_time.nanoseconds());
+                
+                //Gets the rotation orientation
+                this->rotation_orientation = rand()%3 +1;
+            }
+
+            //Checks if the robot has rotated for enough time
+            if(now()- this->rotate_start > rotate_time)
+                this->robot_stopped=false;
+
+            //Rotation orientation
+            if(this->rotation_orientation%2==0)
+                msg.angular.z=2;
+            else
+                msg.angular.z=-2;
+        }
+        
+        return msg;
+    }
+    
+    void laserCallback(const sensor_msgs::msg::LaserScan::SharedPtr msg) 
+    {
+        this->obstacle_distance = *std::min_element(msg->ranges.begin(),msg->ranges.end());
+    }
+
+public:
+    ReactiveController() : Node("reactive_controller")
+    {
+        cmd_vel_pub_=this->create_publisher<geometry_msgs::msg::Twist>("/stage/cmd_vel",10);
+        laser_scan_sub_=this->create_subscription<sensor_msgs::msg::LaserScan>("/scan",10,std::bind(&ReactiveController::laserCallback,this,std::placeholders::_1));
+        timer_ = this->create_wall_timer(100ms, std::bind(&ReactiveController::run, this));
+    }
+
+    void run(){
+        // Send messages in a loop
+        // Calculate the command to apply
+        auto msg = calculateCommand();
+
+        // Publish the new command
+        this->cmd_vel_pub_->publish(msg);
+    }
+};
+
+int main(int argc, char *argv[])
+{
+    srand(time(0));
+
+    rclcpp::init(argc,argv);
+    rclcpp::spin(std::make_shared<ReactiveController>());
+    rclcpp::shutdown();
+    return 0;
+}
