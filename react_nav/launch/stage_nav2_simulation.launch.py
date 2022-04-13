69a9ff8802d6968804e6c08d212aaf5ceeb4d9b8
João Pedro Castilho <33908859+ccpjboss@users.noreply.github.com>
2022-04-13 15:57:27 +0100

    Update stage_nav2_simulation.launch.py

--- react_nav/launch/stage_nav2_simulation.launch.py
+++ react_nav/launch/stage_nav2_simulation.launch.py
@@ -20,8 +20,8 @@ def generate_launch_description():
         'params',
         default='/home/ccpjboss/ros2_test/src/ros2_react/react_nav/params/nav2_params.yaml')
 
-    #nav2_launch_dir = os.path.join(get_package_share_directory('nav2_bringup'), 'launch')
-    nav2_launch_dir = "/home/ccpjboss/ros2_test/src/navigation2/nav2_bringup/bringup/launch"
+    nav2_launch_dir = os.path.join(get_package_share_directory('nav2_bringup'), 'launch')
+    #nav2_launch_dir = "/home/ccpjboss/ros2_test/src/navigation2/nav2_bringup/bringup/launch"
 
     react_launch_dir = os.path.join(get_package_share_directory('react_nav'), 'launch')
 
